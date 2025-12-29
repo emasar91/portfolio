27f2b5d7335d4296d480bceccc21e8036db352fa
Emanuel Sarco <emasar91@gmail.com>
2025-12-29 17:31:33 -0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,27 @@ const inter = Inter({ subsets: ['latin'] })
 const metaInfo = getMetaInfo()
 
 export const metadata: Metadata = {
+	metadataBase: new URL('https://emasar-portfolio.vercel.app'),
 	title: metaInfo.title,
 	description: metaInfo.description,
+	openGraph: {
+		title: metaInfo.title,
+		description: metaInfo.description,
+		url: 'https://emasar-portfolio.vercel.app/',
+		siteName: 'Emanuel Sarco Portfolio',
+		images: [
+			{
+				url: '/assets/images/profile.png',
+				width: 1200,
+				height: 630,
+				alt: 'Emanuel Sarco - Software Developer',
+			},
+		],
+		locale: 'es_AR',
+		type: 'website',
+	},
 }
+
 export default function RootLayout({
 	children,
 }: Readonly<{
