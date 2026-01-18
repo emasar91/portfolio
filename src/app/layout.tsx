fdee6e3987c5c65789a9559265ebb7bd168bb369
Emanuel Sarco <emasar91@gmail.com>
2026-01-18 16:46:20 -0300

    cambio mimiatura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 		siteName: 'Emanuel Sarco Portfolio',
 		images: [
 			{
-				url: '/assets/images/profile.png',
+				url: '/assets/images/miniature.png',
 				width: 1200,
 				height: 630,
 				alt: 'Emanuel Sarco - Software Developer',
